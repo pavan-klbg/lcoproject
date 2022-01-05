7e3d45d4337670a6287c2ec1f5e48e0c4656671a
pavan-klbg <allurpavan111@gmail.com>
2022-01-05 20:57:33 +0530

    nav bar loop added

--- src/components/reuseable/Navbar.js
+++ src/components/reuseable/Navbar.js
@@ -4,8 +4,44 @@ import logo from '../../images/logo.png';
 import {FaCartArrowDown} from 'react-icons/fa'
 
 
+
+
 export default class Navbar extends Component {
+    state={
+        navbarState:false,
+        navbarClass:"collapse navbar-collapse",
+        menus:[{
+            id:1,
+            text:"Home",
+            url:"/",
+        },{
+            id:2,
+            text:"About",
+            url:"/about",
+
+        },{
+            id:3,
+            text:"Services",
+            url:"/services",
+        },{
+            id:4,
+            text:"Contact",
+            url:"/contact",
+        }]
+    }
+    myToggler=()=>{
+        this.state.navbarState?this.setState({
+            navbarState:false,
+            navbarClass:"collapse navbar-collapse"
+        }):this.setState({
+            navbarState:true,
+            navbarClass:"collapse navbar-collapse show"
+        })
+        
+    }
+
     render() {
+        console.log(this.state.navbarState)
         return (
             <nav className='navbar navbar-expand-sm bg-theme text-white'>
               <Link to ="/" className='navbar-brand ml-5'>
@@ -13,20 +49,22 @@ export default class Navbar extends Component {
                 </Link>  
                 <button 
                 className="navbar-toggler"
-                type="button"
+                type="button" onClick={this.myToggler}
                 >
                     <span className='text-white'>menu</span>
                 </button>
-                <div className='collapse navbar-collapse show'>
+                <div className={this.state.navbarClass}>
                     <ul className='navbar-nav ml-auto mr-5'>
-                        <li className="nav-item">
-                            <Link to="/" className='nav-link text-white'>Home</Link>
-                            
+                        {this.state.menus.map(menu=>{
+                            return (
+                        <li key={menu.id} className="nav-item">
+                            <Link to={menu.url} className='nav-link text-white'>
+                                {menu.text}
+                            </Link>    
                         </li>
-                        <li className="nav-item">
-                            <Link to="/" className='nav-link text-white'>About us</Link>
-                            
-                        </li>
+                            )
+                        })}
+
                         <li className="nav-item">
                             <Link to="/" className='nav-link text-white'>
                                 <FaCartArrowDown className='cart-icon'/>
